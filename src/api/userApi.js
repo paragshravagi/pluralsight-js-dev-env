9af8371a424129dac64bab07ebf1801fe434f60e
Shravagi <pshravag@ad.harman.com>
2018-08-08 10:25:33 +0530

    Added support for HTTP calls and mocking them using JSON server and JSON Schema Faker

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,29 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
+
 
 export function getUsers(){
   return get('users');
 }
 
+export function deleteUser(id)
+{
+  return del(`users/${id}`);
+}
+
 function get(url){
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+//Cant call function delete since it is a reserved word
+function del(url)
+{
+  const request = new Request(baseUrl + url, {method : 'DELETE'});
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 getUsers().then( result => {
     let usersBody = "";
@@ -8,12 +8,29 @@ getUsers().then( result => {
       usersBody += `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
-      <td>${user.id}</td>
-      <td>${user.id}</td>
-      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
       </tr>`
 
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    //Must use Array.from to create a real array from a DOM Collection
+    //getElementsByClassName only returns an "array like" object
+
+    Array.from(deleteLinks, link => {
+
+        link.onclick = function(event){
+          const element = event.target;
+          event.preventDefault();
+          deleteUser(element.attributes["data-id"].value);
+          const row = element.parentNode.parentNode;
+          row.parentNode.removeChild(row);
+        };
+    });
+
 });
